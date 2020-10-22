813c33cbb44fed31c7ac271ce423eab41e06cb25
Yanbo Jiang <j654563613@icloud.com>
2020-10-22 18:46:53 -0400

    testmark

--- GoogleMap_demo/ViewController.swift
+++ GoogleMap_demo/ViewController.swift
@@ -6,11 +6,21 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
         // Do any additional setup after loading the view.
     }
 
