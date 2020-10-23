2968763939e1f0f8ae876dbd10f56da150b830d4
bestkobyna <36083088+bestkobyna@users.noreply.github.com>
2020-10-22 22:31:51 -0400

    locate

--- GoogleMap_demo/ViewController.swift
+++ GoogleMap_demo/ViewController.swift
@@ -10,9 +10,18 @@ import GoogleMaps
 import GooglePlaces
 
 class ViewController: UIViewController {
-
+    
+    //instandtiate a location manager
+    private let locationManager = CLLocationManager()
+    @IBOutlet weak var Map: GMSMapView!
+    @IBOutlet weak var address: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        Map.delegate = self
+        /*
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
@@ -21,9 +30,61 @@ class ViewController: UIViewController {
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
+        */
         // Do any additional setup after loading the view.
+    }
+    
+    
+    private func reverseGeocodeCoordinate(_ coordinate: CLLocationCoordinate2D) {
+      let geocoder = GMSGeocoder()
+      geocoder.reverseGeocodeCoordinate(coordinate) { response, error in
+        guard let address = response?.firstResult(), let lines = address.lines else {
+          return
+        }
+        self.address.text = lines.joined(separator: "\n")
+        UIView.animate(withDuration: 0.25) {
+          self.view.layoutIfNeeded()
+        }
+      }
     }
 
 
 }
 
+// MARK: - CLLocationManagerDelegate
+//1
+extension ViewController: CLLocationManagerDelegate {
+  // 2
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    // 3
+    guard status == .authorizedWhenInUse else {
+      return
+    }
+    // 4
+    locationManager.startUpdatingLocation()
+      
+    //5
+    Map.isMyLocationEnabled = true
+    Map.settings.myLocationButton = true
+  }
+  
+  // 6
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let location = locations.first else {
+      return
+    }
+      
+    // 7
+    Map.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+      
+    // 8
+    locationManager.stopUpdatingLocation()
+  }
+}
+
+// MARK: - GMSMapViewDelegate
+extension ViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+      reverseGeocodeCoordinate(position.target)
+    }
+}
